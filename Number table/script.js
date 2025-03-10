64ce3510bdb32d55deec889469e0b6f84e03d1c3
Shawon <shawonh986@gmail.com>
2025-03-11 01:21:25 +0800

    1 row creation done

---
+++ Number table/script.js
@@ -0,0 +1,39 @@
+const inputField = document.getElementById('input-number');
+const tableBody = document.getElementById('table-body');
+const generateButton = document.getElementById('generate');
+
+
+generateButton.addEventListener('click', function(){
+    const number = parseFloat(inputField.value);
+    generateTable(number);
+});
+
+function generateTable(number){
+    const tableRow = generateRow(number,1);
+    tableBody.appendChild(tableRow);
+} 
+
+
+function generateRow(number, i){
+    const tableRow = document.createElement('tr');
+    const cell1 = document.createElement('td');
+    const cell2 = document.createElement('td');
+    const cell3 = document.createElement('td');
+    const cell4 = document.createElement('td');
+    const cell5 = document.createElement('td');
+
+    cell1.innerText = number;
+    cell2.innerText = "x";
+    cell3.innerText = i;
+    cell4.innerText = "=";
+    cell5.innerText = number*i;
+
+    tableRow.appendChild(cell1);
+    tableRow.appendChild(cell2);
+    tableRow.appendChild(cell3);
+    tableRow.appendChild(cell4);
+    tableRow.appendChild(cell5);
+
+    
+    return tableRow;
+}
