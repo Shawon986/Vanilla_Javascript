0b3a85894e6e15f2561c9a63ce008e7f40bee1c3
Shawon <shawonh986@gmail.com>
2025-03-12 01:31:51 +0800

    row generate function table generate function added

---
+++ Number table 1-10/script.js
@@ -0,0 +1,43 @@
+const inputField = document.getElementById('input-number');
+const generateButton = document.getElementById('generate');
+const firstRow = document.getElementById('first-row');
+const secondRow = document.getElementById('second-row');
+
+
+generateButton.addEventListener('click', function(){
+    for(let number = 1; number<=10; number++){
+        const numberTable = getTable(number);
+
+        if(number<=5){
+            firstRow.appendChild(numberTable);
+        }else{
+            secondRow.appendChild(numberTable);
+        }
+    }
+});
+
+function getTable(number){
+    const numberTable = document.createElement('table');
+    numberTable.classList = 'table-auto border border-slate-700 w-full';
+    const tableBody = document.createElement('tbody');
+
+    for(let row = 1; row<=10; row++){
+        const tableRow = getTableRow(number, row);
+        tableBody.appendChild(tableRow);
+    }
+
+
+
+    numberTable.appendChild(tableBody);
+
+    return numberTable;
+};
+
+
+function getTableRow(number,row){
+    const cells =[];
+    for(let cell = 1; cell<=5; cell++){
+        const cell = document.createElement('td');
+        cells.push(cell);
+    }
+}
