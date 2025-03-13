f38ec420b204629000e0e3756d8e0dd95f2881c9
Shawon <shawonh986@gmail.com>
2025-03-13 23:51:05 +0800

    compare funcionality done

---
+++ Number comparetor/script.js
@@ -0,0 +1,22 @@
+const firstInput = document.getElementById('inputOne');
+const secondInput = document.getElementById('inputTwo');
+const compareButton = document.getElementById('buttonOne');
+const resetButton = document.getElementById('buttonTwo');
+const resultPlace = document.getElementById('display');
+
+
+compareButton.addEventListener('click', compare);
+
+function compare(){
+    const firstValue = parseInt(firstInput.value);
+    const secondValue = parseInt(secondInput.value);
+
+    if(firstValue>secondValue){
+        resultPlace.innerText = 'First number is greater than second number'
+    }else if(firstValue<secondValue){
+        resultPlace.innerText = 'First number is less than second number'
+    }else{
+        resultPlace.innerText = 'First number and second number are equal'
+    }
+}
+
