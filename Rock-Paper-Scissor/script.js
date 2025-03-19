ee25866647b8a9d1f6c16b63971c10f5fdb15a57
Shawon <shawonh986@gmail.com>
2025-03-19 21:27:14 +0800

    done and use function parameter

--- Rock-Paper-Scissor/script.js
+++ Rock-Paper-Scissor/script.js
@@ -27,58 +27,58 @@ function compare(){
 }
 
 function rock(){
-
-    const computerMove = compare();
-
-    let finalResult = '';
-
-    if(computerMove==="rock"){
-        finalResult = "Tie"
-        resultDisplay.innerText = `You picked Rock , computer picked ${computerMove} and Result : ${finalResult}`
-    }else if(computerMove==="paper"){
-        finalResult = "You loose"
-        resultDisplay.innerText = `You picked Rock , computer picked ${computerMove} and Result : ${finalResult}`
-    }else if(computerMove === "scissor"){
-        finalResult = "You win"
-        resultDisplay.innerText = `You picked Rock , computer picked ${computerMove} and Result : ${finalResult}`
-    }
-    
-
+    playGame('rock');
 }
 
 function paper(){
-    const computerMove = compare();
-
-    let finalResult = '';
-
-    if(computerMove==="paper"){
-        finalResult = "Tie"
-        resultDisplay.innerText = `You picked Paper , computer picked ${computerMove} and Result : ${finalResult}`
-    }else if(computerMove==="scissor"){
-        finalResult = "You loose"
-        resultDisplay.innerText = `You picked Paper , computer picked ${computerMove} and Result : ${finalResult}`
-    }else if(computerMove === "rock"){
-        finalResult = "You win"
-        resultDisplay.innerText = `You picked Paper , computer picked ${computerMove} and Result : ${finalResult}`
-    }
+    playGame('paper');
 }
 
 function scissor(){
+    playGame('scissor');
+}
+
+
+function playGame(playerMove){
 
     const computerMove = compare();
-
     let finalResult = '';
 
-    if(computerMove==="scissor"){
-        finalResult = "Tie"
-        resultDisplay.innerText = `You picked Scissor , computer picked ${computerMove} and Result : ${finalResult}`
-    }else if(computerMove==="paper"){
-        finalResult = "You Win"
-        resultDisplay.innerText = `You picked Scissor , computer picked ${computerMove} and Result : ${finalResult}`
-    }else if(computerMove === "rock"){
-        finalResult = "You Loose"
-        resultDisplay.innerText = `You picked Scissor , computer picked ${computerMove} and Result : ${finalResult}`
+    if(playerMove === 'scissor'){
+        if(computerMove==="scissor"){
+            finalResult = "Tie"
+            resultDisplay.innerText = `You picked Scissor , computer picked ${computerMove} and Result : ${finalResult}`
+        }else if(computerMove==="paper"){
+            finalResult = "You Win"
+            resultDisplay.innerText = `You picked Scissor , computer picked ${computerMove} and Result : ${finalResult}`
+        }else if(computerMove === "rock"){
+            finalResult = "You Loose"
+            resultDisplay.innerText = `You picked Scissor , computer picked ${computerMove} and Result : ${finalResult}`
+        }
+    }else if(playerMove === 'paper'){
+        if(computerMove==="paper"){
+            finalResult = "Tie"
+            resultDisplay.innerText = `You picked Paper , computer picked ${computerMove} and Result : ${finalResult}`
+        }else if(computerMove==="scissor"){
+            finalResult = "You loose"
+            resultDisplay.innerText = `You picked Paper , computer picked ${computerMove} and Result : ${finalResult}`
+        }else if(computerMove === "rock"){
+            finalResult = "You win"
+            resultDisplay.innerText = `You picked Paper , computer picked ${computerMove} and Result : ${finalResult}`
+        }
+    }else if(playerMove === 'rock'){
+        if(computerMove==="rock"){
+            finalResult = "Tie"
+            resultDisplay.innerText = `You picked Rock , computer picked ${computerMove} and Result : ${finalResult}`
+        }else if(computerMove==="paper"){
+            finalResult = "You loose"
+            resultDisplay.innerText = `You picked Rock , computer picked ${computerMove} and Result : ${finalResult}`
+        }else if(computerMove === "scissor"){
+            finalResult = "You win"
+            resultDisplay.innerText = `You picked Rock , computer picked ${computerMove} and Result : ${finalResult}`
+        }
     }
+    
 }
 
 
